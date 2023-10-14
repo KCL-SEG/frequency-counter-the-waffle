e5dd5f44323a08c05ea736e297c06642df09e982
the-waffle <43791700+the-waffle@users.noreply.github.com>
2023-10-14 19:15:14 +0100

    Implemented frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,12 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
-def frequencies(items):
-    frequencies = {}
-    # Your code goes here
+def frequencies(items : list[str]):
+    frequencies : dict[str, int] = dict()
+    for item in items:
+        item = str(item)
+        if item not in frequencies.keys():
+            frequencies[item] = 1
+        else:
+            frequencies[item] += 1
     return frequencies
